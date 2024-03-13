20d93f4473ec44d0c8d01fcd5eb4ec5425c42a0c
Ryan Chandler <ryangjchandler@gmail.com>
2024-03-13 15:27:30 +0000

    feature: support proxying requests

--- src/EngagingNetworks.php
+++ src/EngagingNetworks.php
@@ -5,12 +5,18 @@ namespace C6Digital\EngagingNetworks;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 use C6Digital\EngagingNetworks\Exceptions\AuthenticationException;
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Support\Traits\Conditionable;
 
 class EngagingNetworks
 {
+    use Conditionable;
+
     protected string $baseUrl = 'https://ca.engagingnetworks.app/ens/service';
 
     protected ?string $token = null;
+
+    protected ?string $proxy = null;
 
     public function __construct(
         protected string $key,
@@ -19,6 +25,13 @@ class EngagingNetworks
     public function setBaseUrl(string $url): static
     {
         $this->baseUrl = $url;
+
+        return $this;
+    }
+
+    public function proxy(string $proxy): static
+    {
+        $this->proxy = $proxy;
 
         return $this;
     }
@@ -39,7 +52,7 @@ class EngagingNetworks
             ->post('https://ca.engagingnetworks.app/ens/service/authenticate')
             ->json();
 
-        if (! $response['ens-auth-token'] && isset($response['message'])) {
+        if (! isset($response['ens-auth-token']) && isset($response['message'])) {
             throw new AuthenticationException('Failed to authenticate. [Message] ' . $response['message']);
         }
 
@@ -48,25 +61,28 @@ class EngagingNetworks
 
     protected function get(string $endpoint, array $data = []): Response
     {
-        $this->authenticate();
-
-        return Http::asJson()
-            ->acceptJson()
-            ->withHeaders([
-                'ens-auth-token' => $this->token,
-            ])
-            ->get($this->baseUrl . $endpoint, $data);
+        return $this->baseClient()->get($endpoint, $data);
     }
 
     protected function post(string $endpoint, array $data = []): Response
     {
+        return $this->baseClient()->post($endpoint, $data);
+    }
+
+    protected function baseClient(): PendingRequest
+    {
         $this->authenticate();
 
         return Http::asJson()
+            ->baseUrl($this->baseUrl)
             ->acceptJson()
             ->withHeaders([
                 'ens-auth-token' => $this->token,
             ])
-            ->post($this->baseUrl . $endpoint, $data);
+            ->when($this->proxy, function (PendingRequest $request) {
+                return $request->withOptions([
+                    'proxy' => $this->proxy,
+                ]);
+            });
     }
 }
